ed3120feb28544d1feca9c7d1299362b835e05e6
VNavrockij <vitalik.nav@gmail.com>
2022-10-28 15:49:37 +0300

    add: context for work

--- MealTime/SceneDelegate.swift
+++ MealTime/SceneDelegate.swift
@@ -16,6 +16,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let nc = window?.rootViewController as! UINavigationController
+        let vc = nc.topViewController as! ViewController
+        let context = (UIApplication.shared.delegate as? AppDelegate)?.coreDataStack.persistentContainer.viewContext
+        vc.context = context
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
