4c5f9a52912c90119e4037c486e326f21c5e79d9
VNavrockij <vitalik.nav@gmail.com>
2022-10-28 16:01:34 +0300

    add: two entities with relationship

---
+++ CoreDataEntities/Meal+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Vitalii Navrotskyi on 28.10.2022.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
